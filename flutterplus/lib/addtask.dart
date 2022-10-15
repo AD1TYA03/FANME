ebb624221e9076ba438135f92c9801dde1908fd9
ADITYA KUMAR SINGH <90272291+AD1TYA03@users.noreply.github.com>
2022-10-15 11:10:08 +0530

    Create addtask.dart

---
+++ flutterplus/lib/addtask.dart
@@ -0,0 +1,35 @@
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AddTaskWidget extends StatefulWidget {
+  const AddTaskWidget({Key? key}) : super(key: key);
+
+  @override
+  _AddTaskWidgetState createState() => _AddTaskWidgetState();
+}
+
+class _AddTaskWidgetState extends State<AddTaskWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Align(
+            alignment: AlignmentDirectional(0, 0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
