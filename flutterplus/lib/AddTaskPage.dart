b7321b4528716bef1a7f924b01f55c79637f3b7d
ADITYA KUMAR SINGH <90272291+AD1TYA03@users.noreply.github.com>
2022-10-15 11:11:54 +0530

    Create AddTaskPage.dart

---
+++ flutterplus/lib/AddTaskPage.dart
@@ -0,0 +1,398 @@
+import '../flutter_flow/flutter_flow_drop_down.dart';
+import '../flutter_flow/flutter_flow_icon_button.dart';
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import '../flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AddTaskPageWidget extends StatefulWidget {
+  const AddTaskPageWidget({Key? key}) : super(key: key);
+
+  @override
+  _AddTaskPageWidgetState createState() => _AddTaskPageWidgetState();
+}
+
+class _AddTaskPageWidgetState extends State<AddTaskPageWidget> {
+  String? teamSelectValue;
+  TextEditingController? shortBioController;
+  TextEditingController? userNameController;
+  String? userSelectValue;
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    shortBioController = TextEditingController();
+    userNameController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    shortBioController?.dispose();
+    userNameController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Create Task',
+          style: FlutterFlowTheme.of(context).title2.override(
+                fontFamily: 'Outfit',
+                color: Color(0xFF14181B),
+                fontSize: 28,
+                fontWeight: FontWeight.normal,
+              ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 12, 0),
+            child: FlutterFlowIconButton(
+              borderColor: Colors.transparent,
+              borderRadius: 30,
+              buttonSize: 48,
+              icon: Icon(
+                Icons.close_rounded,
+                color: Color(0xFF57636C),
+                size: 30,
+              ),
+              onPressed: () async {
+                context.pop();
+              },
+            ),
+          ),
+        ],
+        centerTitle: false,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Form(
+            key: formKey,
+            autovalidateMode: AutovalidateMode.disabled,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+                      child: TextFormField(
+                        controller: userNameController,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          labelText: 'Task Name',
+                          labelStyle:
+                              FlutterFlowTheme.of(context).title3.override(
+                                    fontFamily: 'Outfit',
+                                    color: Color(0xFF57636C),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFFF1F4F8),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFFF1F4F8),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0x00000000),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0x00000000),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          contentPadding:
+                              EdgeInsetsDirectional.fromSTEB(20, 32, 20, 12),
+                        ),
+                        style: FlutterFlowTheme.of(context).title3.override(
+                              fontFamily: 'Outfit',
+                              color: Color(0xFF14181B),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            ),
+                        textAlign: TextAlign.start,
+                        maxLines: 1,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+                      child: TextFormField(
+                        controller: shortBioController,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          hintText: 'Enter description here...',
+                          hintStyle:
+                              FlutterFlowTheme.of(context).bodyText2.override(
+                                    fontFamily: 'Outfit',
+                                    color: Color(0xFF57636C),
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFFF1F4F8),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFFF1F4F8),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0x00000000),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0x00000000),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          contentPadding:
+                              EdgeInsetsDirectional.fromSTEB(20, 32, 20, 12),
+                        ),
+                        style: FlutterFlowTheme.of(context).bodyText1.override(
+                              fontFamily: 'Outfit',
+                              color: Color(0xFF14181B),
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                            ),
+                        textAlign: TextAlign.start,
+                        maxLines: 4,
+                        keyboardType: TextInputType.multiline,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
+                      child: FlutterFlowDropDown(
+                        options: ['Team 1', 'Team 2', 'Team 3'],
+                        onChanged: (val) =>
+                            setState(() => teamSelectValue = val),
+                        width: double.infinity,
+                        height: 60,
+                        textStyle:
+                            FlutterFlowTheme.of(context).bodyText1.override(
+                                  fontFamily: 'Outfit',
+                                  color: Color(0xFF14181B),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                        hintText: 'Select Team',
+                        icon: Icon(
+                          Icons.keyboard_arrow_down_rounded,
+                          color: Color(0xFF57636C),
+                          size: 15,
+                        ),
+                        fillColor: Colors.white,
+                        elevation: 2,
+                        borderColor: Color(0xFFF1F4F8),
+                        borderWidth: 2,
+                        borderRadius: 8,
+                        margin: EdgeInsetsDirectional.fromSTEB(24, 4, 12, 4),
+                        hidesUnderline: true,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
+                      child: FlutterFlowDropDown(
+                        options: ['Not Started', 'In Progress', 'Complete'],
+                        onChanged: (val) =>
+                            setState(() => userSelectValue = val),
+                        width: double.infinity,
+                        height: 60,
+                        textStyle:
+                            FlutterFlowTheme.of(context).bodyText1.override(
+                                  fontFamily: 'Outfit',
+                                  color: Color(0xFF14181B),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                        hintText: 'Select Status',
+                        icon: Icon(
+                          Icons.keyboard_arrow_down_rounded,
+                          color: Color(0xFF57636C),
+                          size: 15,
+                        ),
+                        fillColor: Colors.white,
+                        elevation: 2,
+                        borderColor: Color(0xFFF1F4F8),
+                        borderWidth: 2,
+                        borderRadius: 8,
+                        margin: EdgeInsetsDirectional.fromSTEB(24, 4, 12, 4),
+                        hidesUnderline: true,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width * 0.44,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8),
+                                border: Border.all(
+                                  color: Color(0xFFF1F4F8),
+                                  width: 2,
+                                ),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    12, 5, 12, 5),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Start Date',
+                                      style: FlutterFlowTheme.of(context)
+                                          .bodyText2
+                                          .override(
+                                            fontFamily: 'Outfit',
+                                            color: Color(0xFF57636C),
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                    ),
+                                    Icon(
+                                      Icons.date_range_outlined,
+                                      color: Color(0xFF57636C),
+                                      size: 24,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.44,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
+                                color: Color(0xFFF1F4F8),
+                                width: 2,
+                              ),
+                            ),
+                            child: Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(12, 5, 12, 5),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Due Date',
+                                    style: FlutterFlowTheme.of(context)
+                                        .bodyText2
+                                        .override(
+                                          fontFamily: 'Outfit',
+                                          color: Color(0xFF57636C),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                  ),
+                                  Icon(
+                                    Icons.date_range_outlined,
+                                    color: Color(0xFF57636C),
+                                    size: 24,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 16),
+                  child: FFButtonWidget(
+                    onPressed: () async {
+                      if (formKey.currentState == null ||
+                          !formKey.currentState!.validate()) {
+                        return;
+                      }
+
+                      context.pushNamed(
+                        'HomePage',
+                        extra: <String, dynamic>{
+                          kTransitionInfoKey: TransitionInfo(
+                            hasTransition: true,
+                            transitionType: PageTransitionType.leftToRight,
+                          ),
+                        },
+                      );
+                    },
+                    text: 'Create Task',
+                    options: FFButtonOptions(
+                      width: 270,
+                      height: 50,
+                      color: Color(0xFF4B39EF),
+                      textStyle:
+                          FlutterFlowTheme.of(context).subtitle1.override(
+                                fontFamily: 'Outfit',
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                              ),
+                      elevation: 3,
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
